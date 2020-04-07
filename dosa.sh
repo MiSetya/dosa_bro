1ef966f1cc5746d838281e2cfd9fe2a9eccf9133
Jimmi Setiawan <53130905+MiSetya@users.noreply.github.com>
2020-04-07 20:58:20 +0700

    Add files via upload

---
+++ dosa.sh
@@ -0,0 +1,90 @@
+#recode ae gpp tol, lagian iseng doang
+#gabut by ./MiSetya
+#required bash
+#      pkg install bash
+
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+endc='\E[0m'
+enda='\033[0m'
+
+clear
+echo ""
+echo $me"(        )  (                     (   (     "
+echo $me" )\ )  ( /(  )\ )   (              )\ ))\ )  "
+echo $me"(()/(  )\())(()/(   )\      (   ( (()/(()/(  "
+echo $me" /(_))((_)\  /(_)|(((_)(    )\  )\ /(_))(_)) "
+echo $me"(_))_   ((_)(_))  )\ _ )\  ((_)((_|_))(_))   "
+echo $pu"|   \ / _ \/ __| (_)_\(_) \ \ / /|_ _| _ \  "
+echo $pu"| |) | (_) \__ \  / _ \    \ V /  | ||  _/  "
+echo $pu"|___/ \___/|___/ /_/ \_\    \_/  |___|_|    "
+echo $i"                       MAKE DOSA TO EASY FOR YOU"
+echo $i"HOPEFULLY YOU USE VPN BEFORE MAKE DOSA"
+echo ""
+echo $pu".::::::::::::::::::::::::.
+         ::                          . .
+         :: $i Author : ./MiSetya$pu     ''.''
+         :: $i Fungsi : Buat Dosa$pu      '''' croot'Zz
+         :: $i Create : 07/04/2020$pu    .''''. Ver.1
+         ::                        '..'...'
+'::::::::::::::::::::::::'
+     ::
+     :: $pu 1.$i Nekopoi $me($pu Heaven of vvibu$me )$pu
+     :: $pu 2.$i XNXX$pu
+     :: $pu 3.$i Pornhub$pu
+     ::
+     :: $pu 0.$i Make 0 dosa for you$pu
+     :: $pu 99.$i Contact Author$pu
+     ::
+.::::::::::::::::::::::::."
+echo ""
+read -p "bosen.ngehek?id= : " crot
+
+if [ $crot = '1' ]
+then
+echo""
+echo $me"REDIRECT TO BROWSER..."
+sleep 2
+xdg-open http://nekopoi.care
+fi
+if [ $crot = '2' ]
+then
+echo""
+echo $me"REDIRECT TO BROWSER..."
+sleep 2
+xdg-open http://xnxx.com
+fi
+if [ $crot = '3' ]
+then
+echo""
+echo $me"REDIRECT TO BROWSER..."
+sleep 2
+xdg-open http://pornhub.com
+fi
+if [ $crot = '0' ]
+then
+echo ""
+echo $me"BOOM!! YOU EXIT WITH 0 DOSA"
+sleep 1
+exit
+fi
+if [ $crot = '99' ]
+then
+echo ""
+echo $me"Author is friendly ^_^ nice to meet ya.."
+sleep 2
+xdg-open http://wa.me/+6282386234828?text=i+enjoy+dosa
+exit
+fi
